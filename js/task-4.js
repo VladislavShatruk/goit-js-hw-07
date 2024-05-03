84e5c2f6bccb7032a9be33de94918a542c44dd32
VladislavShatruk <vladshatruk121@gmail.com>
2024-05-03 09:02:40 +0300

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const elements = event.target.elements;
+
+    if (elements.email.value === "" || elements.password.value === "") {
+        alert("All form fields must be filled in")
+    }
+    const info = {
+    email: elements.email.value,
+    password: elements.password.value,
+    }
+    console.log(info);
+    form.reset()
+}
