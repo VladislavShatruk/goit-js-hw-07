cd8a86f312276227f2f6fe935a7c96aae75855d6
VladislavShatruk <vladshatruk121@gmail.com>
2024-05-06 20:04:49 +0300

    changeAll

--- js/task-6.js
+++ js/task-6.js
@@ -4,80 +4,62 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-// const controls = document.querySelector("#controls");
-// const boxes = document.querySelector("#boxes");
-// const inputNbr = document.querySelector('input[type="number"]');
-// // const actions = document.querySelectorAll("#controls button")
 
-// // actions[0].dataset.create = "create";
-// // actions[1].dataset.destroy= "destroy";
-
-// const btnCreate = document.querySelector("button[data-create]");
-// const btnDelete = document.querySelector("button[data-destroy]");
-
-// // console.log(inputNbr);
-// inputNbr.addEventListener("input", createBoxes);
-// btnCreate.addEventListener("click", handleCreate);
-// btnDelete.addEventListener("click", handleDelete);
-
-// function createBoxes(amount) {
-//   const arr = [];
-
-//   for (let i = 0; i < amount; i++) {
-//     const newDiv = document.createElement("div");
-//     const size = 30 + i * 10;
-//     newDiv.style.marginBottom = "8px";
-//     newDiv.style.width = `${size}px`;
-//     newDiv.style.height = `${size}px`;
-//     newDiv.style.backgroundColor = getRandomHexColor();
-
-//     arr.push(newDiv);
-//   }
-//   boxes.append(arr);
-//   console.log(boxes.children);
-// }
-
-// let step = 0;
-
-// function handleCreate() {
-//   if (condition) {
-    
-//   }
-//   const sizeBox = 30;
-//   const newEl = document.createElement("div");
-//   newEl.style.width = `${sizeBox+step}px`;
-//   newEl.style.height= `${sizeBox+step}px`;
-//   newEl.style.backgroundColor = getRandomHexColor();
-//   // boxes.append(newEl);
-//   step += 10;
-// }
-
+const btnCreate = document.querySelector("button[data-create]");
+const btnDelete = document.querySelector("button[data-destroy]");
 const divContainer = document.querySelector("#boxes");
 const inputNbr = document.querySelector('input[type="number"]')
+btnCreate.addEventListener("click", handleCreate);
+btnDelete.addEventListener("click", handleDelete);
 
 
-inputNbr.addEventListener("input", handleInput)
+
+function handleCreate() {
+  boxes.innerHTML = "";
+  const amount = inputNbr.value;
+  createBoxes(amount);
+  inputNbr.value = "";
+}
 
 
-function handleInput(event) {
-  const numberBoxes = event.target.value;
-  // return numberBoxes;
+
+function handleDelete() {
   
-for (let i = 0; i <= numberBoxes; i++) {
-  const div = document.createElement("div");
-  div.classList.add("div-number");
-
-  divContainer.appendChild(div);
-}
+  const amount = inputNbr.value;
+  deleteBoxes(amount);
+  inputNbr.value = "";
 }
 
-console.log(divContainer);
+
+
+function createBoxes(amount) {
+  const arr = [];
+
+  if (amount <= 100 && amount >= 1) {
+
+    for (let i = 0; i < amount; i++) {
+      const newDiv = document.createElement("div");
+      const size = 30 + i * 10;
+      newDiv.style.margin = "5px";
+      newDiv.style.width = `${size}px`;
+      newDiv.style.height = `${size}px`;
+      newDiv.style.backgroundColor = getRandomHexColor();
+      arr.push(newDiv);
+    }
+
+    arr.map(element => boxes.append(element));
+  }
+  return;
+}
 
 
 
+function destroyBoxes(amount) {
 
-
-
-// function handleDelete() {
-//   boxes.innerHTML = "";
-// }
+  if (amount <= 100 && amount >= 1 && amount <= boxes.children.length) {
+    for (let i = 0; i <= amount-1; i++) {
+      boxes.children[0].remove();
+      console.log(boxes.children);
+    }
+  }
+}
